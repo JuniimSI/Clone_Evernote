4e391feb7ec05bedd7e53ca7777d5151e2b515ef
JuniimSI <jrfernandesti@gmail.com>
2021-10-28 13:53:41 -0300

    Ended the project

--- javascript_notes_client/src/components/header_logged/index.js
+++ javascript_notes_client/src/components/header_logged/index.js
@@ -10,6 +10,7 @@ import { faList } from '@fortawesome/free-solid-svg-icons'
 
 function HeaderLogged(props) {
   const [redirectToHome, setRedirectToHome] = useState(false);
+  const [user, setUser] = useState(localStorage.getItem('user'));
 
   const logOut = async () => {
     await UserService.logout();
@@ -58,7 +59,7 @@ function HeaderLogged(props) {
             <Dropdown>
               <Dropdown.Trigger>
                 <Button className="button" color="white" outlined>
-                  <span>Leonardo ▼</span>
+                  <span>{JSON.parse(user)['name']} ▼</span>
                 </Button>
               </Dropdown.Trigger>
               <Dropdown.Menu>
