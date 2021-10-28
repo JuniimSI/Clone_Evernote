82d12a15b63679524124a872c5cdad7342fc6624
JuniimSI <jrfernandesti@gmail.com>
2021-10-28 13:16:05 -0300

    Added endpoints to edit user

--- javascript_notes_client/src/App.js
+++ javascript_notes_client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import "./App.scss";
-import Home from "./screens/home";
 import Routes from './routes';
 
 const App = () => (
--- javascript_notes_client/src/screens/notes/index/index.js
+++ javascript_notes_client/src/screens/notes/index/index.js
@@ -1,14 +1,16 @@
 import React, { Fragment, useState } from 'react';
-import "../../../styles/notes.scss";
 import HeaderLogged from "../../../components/header_logged";
+import "../../../styles/notes.scss";
+
 import Notes from "../../../components/notes";
 
 function NotesScreen() {
+  const [isOpen, setIsOpen] = useState(false)
 
   return(
   <Fragment>
-      <HeaderLogged/>
-      <Notes/>
+      <HeaderLogged setIsOpen={setIsOpen}/>
+      <Notes isOpen={isOpen} setIsOpen={setIsOpen}/>
     </Fragment>
   )
 }
